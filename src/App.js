727441e0c2aef40bccb0e6dcbd74bf0856fcf865
Kara Rainey <karain82@hotmail.com>
2019-01-26 02:26:46 -0600

    added minor animation, too tired to do more tonight...

--- src/App.js
+++ src/App.js
@@ -26,54 +26,55 @@ class App extends Component {
         <div className="App-header">
           <nav>
             <ul className="App-nav-list">
-            <Link to = "/"><li>Home</li></Link>
-            <Link to = "/about"><li>About</li> </Link>
-              <li>Store(coming soon)</li>
+              <Link to = "/"><li>Home</li></Link>
+              <Link to = "/about"><li>About</li> </Link>
+                <li>Store(coming soon)</li>
             </ul>
           </nav>
-          <div>
-            <h1>BURDENEDSTAR 1</h1>
-            <div className="App-tagline">
-              <div>
-                <p>
-                  Gamers don't die, they <span className="App-rage">RAGE</span>{" "}
-                  quit!
-                </p>
+            <div>
+              <h1>BURDENEDSTAR 1</h1>
+                <div className="App-tagline">
+                  <div>
+                    <p>
+                       Gamers don't die, they <span className="App-rage">RAGE</span>{" "}
+                       quit!
+                    </p>
+                  </div>
+                </div>
+            </div>
+              <div className="App-social-links">
+                <div>
+                  <a href="https://www.youtube.com/channel/UCXhSilRTf8KjI-YUBQDOt6g">
+                    <img src={youtube} alt="youtube logo" height="65vh" />
+                  </a>
+                </div>
+                  <div>
+                    <a href="https://www.instagram.com/burdenedstar1/?hl=en">
+                      <img src={insta} alt="instagram logo" height="60vh" />
+                    </a>
+                  </div>
+                    <div>
+                      <a href="https://www.facebook.com/BurdenedStar">
+                        <img src={facebook} alt="facebook logo" height="65vh" />
+                      </a>
+                    </div>
+                      <div>
+                        <a href="https://twitter.com/burdenedStar1">
+                          <img src={twitter} alt="twitter logo" height="60vh" />
+                        </a>
+                      </div>
               </div>
             </div>
-          </div>
-          <div className="App-social-links">
-            <div>
-              <a href="https://www.youtube.com/channel/UCXhSilRTf8KjI-YUBQDOt6g">
-                <img src={youtube} alt="youtube logo" height="65vh" />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.instagram.com/burdenedstar1/?hl=en">
-                <img src={insta} alt="instagram logo" height="60vh" />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.facebook.com/BurdenedStar">
-                <img src={facebook} alt="facebook logo" height="65vh" />
-              </a>
-            </div>
-            <div>
-              <a href="https://twitter.com/burdenedStar1">
-                <img src={twitter} alt="twitter logo" height="60vh" />
-              </a>
-            </div>
-          </div>
-          </div>
-        </header>
-        <div className="App-main-container">
-          {/* <img src="http://worldartsme.com/images/construction-tape-clipart-1.jpg" className="Tape" alt="tape"/> */}
-          <div>
-          </div>
+          </header>
           
-          
-          <Routes />
-        </div>
+          <div className="App-main-container">
+            <div>
+              <div>
+                <h2 className="App-main-title">BURDENEDSTAR1</h2>
+                <Routes />
+              </div>
+            </div>        
+          </div>
       </div>
     );
   }
